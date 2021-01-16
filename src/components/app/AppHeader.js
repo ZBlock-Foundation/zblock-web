4e537dd697850604ec476163da2546bdce2239e4
selcuk sozuer <selcuk.sozuer@gmail.com>
2021-01-16 16:50:19 +0300

    App nav changes

--- src/components/app/AppHeader.js
+++ src/components/app/AppHeader.js
@@ -1,20 +1,70 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // UI Components
-import { Layout, Menu } from "antd";
-
-const { Header } = Layout;
+import { Menu, Drawer, Button } from "antd";
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 const AppHeader = () => {
   return (
-    <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
+    <nav className="menuBar">
+      <div className="logo">
+        <a href="">ZBlock</a>
+      </div>
+      <div className="menuCon">
+        <div className="leftMenu">
+          <Menu mode="horizontal">
+            <Menu.Item key="mail">
+              <Link to="/app">Home</Link>
+            </Menu.Item>
+
+            <Menu.Item key="community">
+              <a href="">Communities</a>
+            </Menu.Item>
+          </Menu>
+        </div>
+        <div className="rightMenu">
+          <Menu mode="horizontal">
+            <Menu.Item key="mail">
+              <a href="">Connect Metamask</a>
+            </Menu.Item>
+          </Menu>
+        </div>
+        <Button className="barsMenu" type="primary">
+          <span className="barsBtn"></span>
+        </Button>
+
+        <Drawer title="Basic Drawer" placement="right">
+          <Menu mode="horizontal">
+            <Menu.Item key="mail">
+              <a href="">Home</a>
+            </Menu.Item>
+            <SubMenu title={<span>Blogs</span>}>
+              <MenuItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </MenuItemGroup>
+              <MenuItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+              </MenuItemGroup>
+            </SubMenu>
+            <Menu.Item key="alipay">
+              <a href="">Contact Us</a>
+            </Menu.Item>
+          </Menu>
+          <Menu mode="horizontal">
+            <Menu.Item key="mail">
+              <a href="">Signin</a>
+            </Menu.Item>
+            <Menu.Item key="app">
+              <a href="">Signup</a>
+            </Menu.Item>
+          </Menu>
+        </Drawer>
+      </div>
+    </nav>
   );
 };
 
